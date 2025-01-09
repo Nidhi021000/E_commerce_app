4e68ee86f37f86cb85e09f43f02623106567ee09
Nidhi Patel <pnidhi8899@gmail.com>
2025-01-10 03:03:57 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import {BrowserRouter} from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
- <BrowserRouter>
-   <ShopContextProvider>
+ <BrowserRouter basename="/E_commerce_app">
+   <ShopContextProvider >
       <App />
    </ShopContextProvider>
  </BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/E_commerce_app"
+  base: "/E-commerce-app"
 })
